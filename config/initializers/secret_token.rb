d9f65998cc11f8c8f2924306936fcc5d8a1f20e8
Jon Couch <joncouch4@hotmail.com>
2014-02-12 09:37:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SurfApp::Application.config.secret_key_base = '9d31d7dbf9d712e277eedc8c571fbcf77b47fe0071434a45d51feff7e0aeac550d4388ad316da738050543177779666a6f33e98dbeac1bf6673519c967736418'
